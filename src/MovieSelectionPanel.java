df6abee7135bb601cd67b4e391c907148f26b66a
ElaDeCode <eladecode+github@gmail.com>
2024-05-08 17:26:48 +0300

    update

--- src/MovieSelectionPanel.java
+++ src/MovieSelectionPanel.java
@@ -1,30 +1,13 @@
 import javax.swing.*;
-
-import java.awt.*;
-import java.util.*;
-import java.awt.event.*;
-import java.time.LocalDate;
-import java.util.List; // Add this import statement
-import javax.swing.JToolBar;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import java.awt.Dimension;
-import java.util.Collections;
-import javax.swing.JComboBox;
-import javax.swing.JPanel;
-import javax.swing.JDialog;
-import java.awt.BorderLayout;
-import javax.swing.SpinnerDateModel;
-import javax.swing.JSpinner;
-import java.util.Calendar;
 import javax.swing.JSpinner.DateEditor;
 
-import java.sql.SQLException; // Add this import statement
-import java.time.LocalDate; // Add this import statement
-import java.util.Map; // Add this import statement
-import java.util.HashMap; // Add this import statement
-
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * Represents a window for selecting movies.
@@ -72,64 +55,35 @@ public class MovieSelectionPanel extends JPanel {
         scrollPane.getVerticalScrollBar().setUnitIncrement(unitIncrement);
         scrollPane.setPreferredSize(new Dimension(300, 300)); // set a preferred size
         add(scrollPane, BorderLayout.CENTER);
-
-        updateMovieList();
-        reapintMoviePanels();
     }
 
-    /**
-     * Creates a panel for a movie.
-     * 
-     * @param movie The movie to create the panel for.
-     * @return The created panel.
-     */
-    private JPanel createMovieBanner(Movie movie) {
-        JPanel moviePanel = new JPanel();
-        moviePanel.setPreferredSize(new Dimension(width - 50, 80));
-        moviePanel.setBackground(Color.WHITE);
-        moviePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        moviePanel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                parent.selectMovie(movie, dateFilter);
-            }
-        });
+    private class MovieBanner extends JPanel{
+        public MovieBanner(Movie movie) {
+            setPreferredSize(new Dimension(width - 50, 80));
+            setBackground(Color.WHITE);
+            setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    parent.selectMovie(movie, dateFilter);
+                }
+            });
 
-        // add movie name
-        JLabel movieName = new JLabel(movie.getName());
-        moviePanel.add(movieName);
-
-        // add movie rating
-        JLabel movieRating = new JLabel("Rating: " + movie.getRating());
-        moviePanel.add(movieRating);
-
-        // add movie release date
-        JLabel movieRelease = new JLabel("Release: " + movie.getReleaseDate().toString());
-        moviePanel.add(movieRelease);
-
-        return moviePanel;
+            JLabel movieName = new JLabel(movie.getName());
+            JLabel movieRating = new JLabel("Rating: " + movie.getRating());
+            JLabel movieRelease = new JLabel("Release: " + movie.getReleaseDate().toString());
+    
+            add(movieName);
+            add(movieRating);
+            add(movieRelease);
+        }
     }
 
-    public void reverseOrder() {
-        Collections.reverse(movieList);
-        reapintMoviePanels();
-    }
-
-    private void updateMovieList() {
-        movieList = Movie.getAllMovies();
-    }
-
-    // private void loadFilteredMovies() {
-        // movieList = DatabaseManager.getRowsFiltered(
-
-    /**
-     * Updates the layout of the movie selection window.
-     */
     public void reapintMoviePanels() {
         movieListingPanel.removeAll();
 
         for (Movie movie : movieList) {
-            JPanel moviePanel = createMovieBanner(movie);
+            JPanel moviePanel = new MovieBanner(movie);
             movieListingPanel.add(moviePanel);
         }
 
